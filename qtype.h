cb93e1bae8ea279d3e31c16610d7db090ae2eb93
jinsol2773 <honest_2773@naver.com>
2025-06-14 17:47:42 +0900

    깊은 복사를 비롯한 추가사항들 구현

--- qtype.h
+++ qtype.h
@@ -1,35 +1,30 @@
-﻿#ifndef _QTYPE_H  // header guard
+﻿#ifndef _QTYPE_H
 #define _QTYPE_H
 
-// ==========이 파일은 수정 가능==========
+#include <atomic>
 
-typedef unsigned int Key;  // 값이 클수록 높은 우선순위
+typedef unsigned int Key;
 typedef void* Value;
 
 typedef struct {
     Key key;
     Value value;
+    int value_size;
 } Item;
 
 typedef struct {
-    bool success;   // true: 성공, false: 실패
+    bool success;
     Item item;
-    // 필드 추가 가능
 } Reply;
 
 typedef struct node_t {
     Item item;
     struct node_t* next;
-    // 필드 추가 가능
 } Node;
 
 typedef struct {
-    Node* head, tail;
-    Node* head_ptr;
-    Node* tail_ptr;
+    Node* head, * tail;
     // 필드 추가 가능
 } Queue;
-
-// 이후 자유롭게 추가/수정: 새로운 자료형 정의 등
 
 #endif
--- queue.cpp
+++ queue.cpp
@@ -1,73 +1,89 @@
-#include <iostream>
-#include "queue.h"
 #include <mutex>
+#include <cstdlib>
+#include <cstring>
+#include "queue.h"
 
 std::mutex mtx;
 
 Queue* init(void) {
     Queue* queue = new Queue;
-    queue->head_ptr = nullptr;
-    queue->tail_ptr = nullptr;
+    queue->head = nullptr;
+    queue->tail = nullptr;
     return queue;
 }
 
 void release(Queue* queue) {
-    Node* current = queue->head_ptr;
+    Node* current = queue->head;
     while (current != nullptr) {
         Node* temp = current;
         current = current->next;
+        if (temp->item.value) {
+            free(temp->item.value);
+        }
         delete temp;
     }
     delete queue;
-}
-
-Node* nalloc(Item item) {
-    Node* node = new Node;
-    node->item = item;
-    node->next = nullptr;
-    return node;
-}
-
-void nfree(Node* node) {
-    delete node;
-}
-
-Node* nclone(Node* node) {
-    return nalloc(node->item);
 }
 
 Reply enqueue(Queue* queue, Item item) {
     std::lock_guard<std::mutex> lock(mtx);
     Reply reply = { false, item };
 
-    Node* new_node = nalloc(item);
-    if (queue->tail_ptr == nullptr) {
-        queue->head_ptr = queue->tail_ptr = new_node;
+    int raw_value = (int)(intptr_t)(item.value);
+
+    void* new_value = malloc(sizeof(int));
+    if (!new_value) return reply;
+    memcpy(new_value, &raw_value, sizeof(int));
+
+    Node* current = queue->head;
+    while (current != nullptr) {
+        if (current->item.key == item.key) {
+            free(current->item.value);
+            current->item.value = new_value;
+            current->item.value_size = sizeof(int);
+            reply.success = true;
+            reply.item = current->item;
+            return reply;
+        }
+        current = current->next;
+    }
+
+    Node* new_node = new Node;
+    new_node->item.key = item.key;
+    new_node->item.value = new_value;
+    new_node->item.value_size = sizeof(int);
+    new_node->next = nullptr;
+
+    if (queue->tail == nullptr) {
+        queue->head = queue->tail = new_node;
     }
     else {
-        queue->tail_ptr->next = new_node;
-        queue->tail_ptr = new_node;
+        queue->tail->next = new_node;
+        queue->tail = new_node;
     }
 
     reply.success = true;
+    reply.item = new_node->item;
     return reply;
 }
 
 Reply dequeue(Queue* queue) {
     std::lock_guard<std::mutex> lock(mtx);
-    Reply reply = { false, {0, nullptr} };
+    Reply reply = { false, {0, nullptr, 0} };
 
-    if (queue->head_ptr == nullptr) {
+    if (queue->head == nullptr)
         return reply;
-    }
 
-    Node* temp = queue->head_ptr;
-    reply.item = temp->item;
-    queue->head_ptr = queue->head_ptr->next;
-    if (queue->head_ptr == nullptr)
-        queue->tail_ptr = nullptr;
+    Node* temp = queue->head;
+    reply.item.key = temp->item.key;
+    reply.item.value = temp->item.value;
+    reply.item.value_size = temp->item.value_size;
 
-    nfree(temp);
+    queue->head = queue->head->next;
+    if (queue->head == nullptr)
+        queue->tail = nullptr;
+
+    delete temp;
     reply.success = true;
     return reply;
 }
@@ -76,10 +92,22 @@ Queue* range(Queue* queue, Key start, Key end) {
     std::lock_guard<std::mutex> lock(mtx);
     Queue* new_queue = init();
 
-    Node* current = queue->head_ptr;
+    Node* current = queue->head;
     while (current != nullptr) {
         if (current->item.key >= start && current->item.key <= end) {
-            enqueue(new_queue, current->item);
+            void* copied_value = malloc(current->item.value_size);
+            if (!copied_value) {
+                current = current->next;
+                continue;
+            }
+            memcpy(copied_value, current->item.value, current->item.value_size);
+
+            Item new_item;
+            new_item.key = current->item.key;
+            new_item.value = copied_value;
+            new_item.value_size = current->item.value_size;
+
+            enqueue(new_queue, new_item);
         }
         current = current->next;
     }
